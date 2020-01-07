bd5f47fc3d8583c4534d380342688906017fdd80
王晓泉 <ougyosen@team-lab.com>
2020-01-07 19:43:52 +0900

    init normal version

---
+++ code.js
@@ -0,0 +1,129 @@
+
+/***********************************/
+/*http://www.layabox.com  2017/12/12*/
+/***********************************/
+var shareText = null;
+var videoPath = null;
+var loading = false;
+let onTap = function(touches, changedTouches, timeStamp){
+	console.log(touches.changedTouches[0])
+	if(!loading && touches.changedTouches[0].clientX < 300 && touches.changedTouches[0].clientY <100){
+		loading = true;
+		startRequest();	
+	}
+}
+tt.onTouchEnd(onTap);
+let canvas = tt.createCanvas();
+
+let ctx = canvas.getContext('2d');
+createPath(0,0,300,100);
+drawText();
+function startRequest(){
+
+showToast(`加载中...`);
+let task = tt.request({
+    url: 'https://dzy.mplayad.com/myphone/functions/getGameVideo',
+    data: {
+        game:"zhaliepaopao"
+    },
+    header: {
+		"X-Parse-Application-Id":"apollo_online",
+        'content-type': 'application/json'
+    },
+	method:"POST",
+    success (res) {
+        
+		shareText = res.data.result.text;
+		setJianjiban(shareText);
+		tt.downloadFile({
+			url: res.data.result.url,
+			success (res) {
+				showToast(`download完成`);
+				videoPath = res.tempFilePath;
+				onStartClick();
+				loading=false;
+			},
+			fail (res) {
+				loading=false;
+				showToast(`下载失败`);
+			}
+		});
+    },
+    fail (res) {
+		loading=false;
+        showToast(`API请求失败`);
+    }
+});
+}
+function onStartClick(){
+		if (videoPath != null){
+		tt.shareAppMessage({
+			channel: 'video',
+			title: '炸裂泡泡龙',
+			desc: "shareText",
+			imageUrl: 'game/shard.png',
+			templateId: '16ee64n9d9dd2qavi8', 
+			query: '',
+			extra: {
+				videoPath: videoPath, // 可替换成录屏得到的视频地址
+				videoTopics: ['炸裂泡泡龙'],
+				createChallenge: true
+			},
+			success() {
+				showToast('分享视频成功');
+				exitMiniProgram();
+			},
+			fail(e) {
+				showToast('分享视频失败');
+			}
+			})
+		}
+	}
+	
+	function showToast(text){
+		tt.showToast({
+			title: text,
+			duration: 2000,
+			success (res) {
+				//showToast(`${res}`);
+			},
+			fail (res) {
+				showToast(`showToast调用失败`);
+			}
+		});
+	}
+
+	function setJianjiban(text){
+		tt.setClipboardData({
+			data: text,
+			success (res) {
+				console.log(`setClipboardData调用成功`);
+			},
+			fail (res) {
+				console.log(`setClipboardData调用失败`);
+			}
+		});
+	}
+
+
+function createPath(x, y, width, height, radius) {
+            ctx.rect(0,0,300,100);
+			ctx.fillStyle="#0022ee";
+			ctx.fill();	
+			
+        }
+
+function drawText(){
+     var xoffset = ctx.measureText("开始").width;
+     var x = 0,
+         y = 0;
+     ctx.save();
+     ctx.beginPath();
+     ctx.font = "30px Micosoft yahei";
+     ctx.fillStyle = "#000";
+     ctx.textBaseline = 'middle';
+     ctx.textAlign = 'center';
+     ctx.fillText("开始", x + (300 - xoffset) / 2 + 10, y + (100 - 22) / 2 + 5, 300);
+     ctx.closePath();
+     ctx.restore();
+ }
---
+++ game.js
@@ -0,0 +1,11 @@
+console.log('开发字节跳动小游戏过程中可以参考以下文档:')
+console.log('https://developer.toutiao.com/docs/game/');
+
+let systemInfo = tt.getSystemInfoSync()
+
+function draw() {  
+       require("code.js");
+
+}
+
+draw();
